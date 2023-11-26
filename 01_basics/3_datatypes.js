3a0f64385fc251350e728aaae1bd8a4e6fb5fe67
Arohi Saxena <68246655+DeviLinux-sudo@users.noreply.github.com>
2023-11-26 08:04:08 +0000

    File Changes Done

---
+++ 01_basics/3_datatypes.js
@@ -0,0 +1,16 @@
+// number ranges from 2 to power 53
+let age = 8;
+// bigint
+// string represented as "xyz"
+let name = "Aarohi";
+// boolean => true/ false
+let isLoggedIn = false
+// null => standalone value i.e representation of empty value
+// undefined => value isnt assigned yet
+let state;
+//symbol => To find uniquness of a component , mostly used in react
+// object => primitive type
+
+console.log(typeof "Aarohi"); //string
+console.log(typeof null); // object
+console.log(typeof undefined);// undefined
