b7bf903d43443950b28b64323268d4134ee98d90
Arohi Saxena <68246655+DeviLinux-sudo@users.noreply.github.com>
2023-11-27 13:35:48 +0000

    Math and it's methods Done

--- 01_basics/8_num_and_maths.js
+++ 01_basics/8_num_and_maths.js
@@ -26,6 +26,24 @@ console.log(hundreds.toLocaleString('en-IN'));
 
 // ***************************Maths*******************************
 
-console.log(Math); // Object within itself and has many properties --> Object [Math] {}
+// console.log(Math); // Object within itself and has many properties --> Object [Math] {}
+// console.log(Math.abs(-4)); // absolute function makes negative value as positive and positive value stays positive. --> 4
+// console.log(Math.round(4.6)); //5
+// console.log(Math.ceil(4.2)); //5
+// console.log(Math.floor(4.9)); //4
+// console.log(Math.min(4,3,6,8)); //3
+// console.log(Math.max(4,3,6,8)); //8
+
+// console.log(Math.random()); // Values always ranges from 0 to 1 ---> 0.3878911870289443
+// console.log(Math.random()*10);// In case , I want my values to range between 0 to 10 , it shifts the number to left --> 7.937481266320168
+// console.log(Math.random()*10 + 1); // to avoid values like 0.0 --> 5.735290786604929
+// console.log((Math.random()*10) + 1); // 10.55063495067321
+
+// If number has to be written, ranging from min to max 
+
+const min = 10;
+const max = 20;
+console.log(Math.floor(Math.random()*(max-min+1))+min); // 11
+
 
 
