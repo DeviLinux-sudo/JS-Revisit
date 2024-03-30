b73a8a83cd97d75c00e373d9f94a97fe5b3a1266
Arohi Saxena <68246655+DeviLinux-sudo@users.noreply.github.com>
2024-03-30 22:03:26 +0000

    Basic take on variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 144553
+let accountEmail = "Aarohi@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur" //not recommended
+
+// Prefer not to use var , due to block and functional scopes
+
+accountEmail = "Saxena@gmail.com"
+accountCity = "Bengaluru"
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,20 @@
-console.log("Hello Aarohi");
+// Javascript Display Properties 
+
+// Writing into an HTML element, using innerHTML
+
+document.getElementById("demo").innerHTML = 5 + 6
+
+// Only for tetsing purposes , document.write ; Never call document.write after the document has finished loading ; it will overwrite the whole document.
+
+document.write(10+9);
+
+//alert()
+
+alert(6+7) 
+
+//console.log
+
+console.log("Ola Aarohi !!")
+
+
+
