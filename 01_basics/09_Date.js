f00daf20fdee4601448f957262f983a361d80767
Arohi Saxena <68246655+DeviLinux-sudo@users.noreply.github.com>
2024-04-03 10:59:24 +0000

    Date Completed

--- 01_basics/09_Date.js
+++ 01_basics/09_Date.js
@@ -8,3 +8,24 @@ console.log(typeof myDate);
 
 let myCreatedDate = new Date(2023, 0 , 23)
 console.log(myCreatedDate.toDateString());
+let anotherCreatedDate = new Date(2023, 0, 23, 5, 3)
+console.log(anotherCreatedDate.toLocaleString());
+
+//Time Stamps 
+
+let timeStamp = Date.now();
+
+console.log(timeStamp);
+console.log(myCreatedDate.getTime());
+console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date();
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
+
+// Customize the Dates more ?
+
+newDate.toLocaleString('default', {
+    weekday : "long"
+})
