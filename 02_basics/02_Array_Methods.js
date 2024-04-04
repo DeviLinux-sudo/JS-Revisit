9b18184f619b5daa0b51edf4d62751f9b4243ec7
Arohi Saxena <68246655+DeviLinux-sudo@users.noreply.github.com>
2024-04-04 06:23:24 +0000

    Array Basic Methods

--- 02_basics/02_Array_Methods.js
+++ 02_basics/02_Array_Methods.js
@@ -36,8 +36,49 @@ const myBoys = ["Ismail", "Krishna", "Ram"];
 const myChildren = myGirls.concat(myBoys);
 console.log(myChildren);
 
-//Array copyWithin() : copies array elements to another position in an array 
-console.log(fruits.copyWithin(2,0)); // copy to index 2, all elements from index 0 // [ 'Banana', ' Mango', 'Banana' ]
+//Array copyWithin() : copies array elements to another position in an array ; overwrites existing values ; doesnt add items to the array ; doesnt change the length of the array
+fruits = ["Banana", "Orange", "Apple", "Mango", "Kiwi"]
+console.log(fruits.copyWithin(2,0)); // copy to index 2, all elements from index 0 // [ 'Banana', 'Orange', 'Banana', 'Orange', 'Apple' ]
+fruits = ["Banana", "Orange", "Apple", "Mango", "Kiwi"]
+console.log(fruits.copyWithin(2,0,2)); //copy to index 2 , elements from index 0 to 2 // [ 'Banana', 'Orange', 'Banana', 'Orange', 'Kiwi' ]
+
+// Flatenning an Array : Converting a multiDimensional Array to One dimensional 
+// flat() : method creates a new array with sub-array elements concatenated to a specified depth ; ES2019 Feature
+
+const myArr = [[1,2],[3,4],[5,6]];
+console.log(myArr.flat()); //[ 1, 2, 3, 4, 5, 6 ]
+
+//splice() : can be used to add new items to an array
+fruits = ["Banana", "Orange", "Apple", "Mango"]
+ // 1st paramter : where new elements should be added ; 2nd parameter : how many elemnts should be removed ; rest of the paramters defines new elements to be added
+ console.log(fruits.splice(2, 2, "Lemon", "Kiwi")); // returns removed elements ; [ 'Apple', 'Mango' ]
+ console.log(fruits); //[ 'Banana', 'Orange', 'Lemon', 'Kiwi' ]
+
+ //To remove elements via splice() ; omit the last two parameters 
+
+ //toSpliced() : Safe way to splice an array without altering the original array 
+ //Difference between splice and tospliced is that new method creates a new array , keeping original array unchanged , while old method altered the original array ; ES2023
+
+ let months = ["Jan","Feb","Mar","Apr"];
+ console.log(months.toSpliced(0,1)); //[ 'Feb', 'Mar', 'Apr' ]
+ console.log(months);//[ 'Jan', 'Feb', 'Mar', 'Apr' ]
+
+ //slice() : slices out a piece of an array into a new array ; doesnt remove any elements
+
+fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+console.log(fruits.slice(3)); //[ 'Apple', 'Mango' ]
+console.log(fruits); //[ 'Banana', 'Orange', 'Lemon', 'Apple', 'Mango' ]
+
+console.log(fruits.slice(1,3)); //[ 'Orange', 'Lemon' ]
+
+
+
+
+
+
+
+
+
 
 
 
